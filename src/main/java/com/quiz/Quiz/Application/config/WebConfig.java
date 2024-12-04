7faa46b5bb73b013b5206fc03e88e2e2f9660bfd
Nikhil Kothare <nikhilkothare12@gmail.com>
2024-12-04 18:38:06 +0530

    deploy ready

--- src/main/java/com/quiz/Quiz/Application/config/WebConfig.java
+++ src/main/java/com/quiz/Quiz/Application/config/WebConfig.java
@@ -9,7 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allows all endpoints
-                .allowedOrigins("http://localhost:8080","http://192.168.149.89:8080","http://127.0.0.1:5500/","http://192.168.149.89:5500") // Allows your frontend origin
+                .allowedOrigins("http://localhost:8080","http://192.168.149.89:8080","http://127.0.0.1:5500/","http://192.168.149.89:5500",
+                        "quizapplicationspringboot-production-e821.up.railway.app") // Allows your frontend origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
